14f55317dbde40790a79ecb047a5913c2348ff62
pascal1129 <feijingjing1994@foxmail.com>
2019-03-25 17:13:28 +0800

    turn to RGB

--- codes/cal_mean_and_std.py
+++ codes/cal_mean_and_std.py
@@ -1,56 +1,57 @@
 # ref：https://zhuanlan.zhihu.com/p/30966663
 # for https://www.kaggle.com/c/human-protein-atlas-image-classification
 
-import pandas as pd
 import numpy as np
+import os
 from PIL import Image
 
-df = pd.read_csv('../input/train.csv')
-# df = df[1:10]
 r = 0 # r mean
 g = 0 # g mean
 b = 0 # b mean
-y = 0
 
 r_2 = 0 # r^2 
 g_2 = 0 # g^2
 b_2 = 0 # b^2
-y_2 = 0
 
 total = 0
 
+root_dir = './image'
+im_names = os.listdir(root_dir)
 
-for i in range(len(df)):
-    filename = '../input/train/' + df.Id.iloc[i]
-    c_r = np.array(Image.open(filename+"_red.png")).astype(np.float32) /255.
-    c_g = np.array(Image.open(filename+"_green.png")).astype(np.float32) /255.
-    c_b = np.array(Image.open(filename+"_blue.png")).astype(np.float32) /255. 
-    c_y = np.array(Image.open(filename+"_yellow.png")).astype(np.float32) /255. 
-
-
-    total += 512*512
+for i,name in enumerate(im_names):
+    im_path = os.path.join(root_dir, name)
+    img = np.array(Image.open(im_path))
+    img = img.astype('float32') / 255.
+    total += img.shape[0] * img.shape[1]
     
-    r += c_r.sum()
-    g += c_g.sum()
-    b += c_b.sum()
-    y += c_y.sum()
-
-    r_2 += (c_r**2).sum()
-    g_2 += (c_g**2).sum()
-    b_2 += (c_b**2).sum()
-    y_2 += (c_y**2).sum()
+    r += img[:, :, 0].sum()
+    g += img[:, :, 1].sum()
+    b += img[:, :, 2].sum()
     
+    r_2 += (img[:, :, 0]**2).sum()
+    g_2 += (img[:, :, 1]**2).sum()
+    b_2 += (img[:, :, 2]**2).sum()
+
     print(i)
 
 r_mean = r / total
 g_mean = g / total
 b_mean = b / total
-y_mean = y / total
 
-r_std = np.sqrt(r_2 / total - r_mean ** 2)
-g_std = np.sqrt(g_2 / total - g_mean ** 2)
-b_std = np.sqrt(b_2 / total - b_mean ** 2)
-y_std = np.sqrt(y_2 / total - y_mean ** 2)
+r_var = r_2 / total - r_mean ** 2
+g_var = g_2 / total - g_mean ** 2
+b_var = b_2 / total - b_mean ** 2
 
-print([r_mean,g_mean,b_mean,y_mean])
-print([r_std, g_std, b_std, y_std])
+r_std = r_var ** 0.5
+g_std = g_var ** 0.5
+b_std = b_var ** 0.5
+
+print(r_mean, g_mean, b_mean)
+print(r_std, g_std, b_std)
+
+
+""" Reference:Imagenet
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+"""
